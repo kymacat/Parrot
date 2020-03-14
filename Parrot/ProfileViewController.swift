4e6f8084177eae36b61b1a2156f7cdbc01237591
Vlad Yandola <vladan2000.9@mail.ru>
2020-03-14 20:24:00 +0300

    Добавил появление режима редактирования профиля при  нажатии на кнопку

--- Parrot/ProfileViewController.swift
+++ Parrot/ProfileViewController.swift
@@ -10,19 +10,25 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-    
     // MARK: - UI
     
+    weak var scrollView: UIScrollView!
+    
     weak var profileImage: UIImageView!
+    
     weak var setProfileImageButton: CustomButton!
     
     weak var nameLabel: UILabel!
     weak var nameTextField: UITextField!
     
+    
     weak var descriptionTextView: UITextView!
     weak var editingDescriptionTextView: UITextView!
     
     weak var editingButton: UIButton!
+    
+    weak var GCDSaveButton: CustomButton!
+    weak var OperationSaveButton: CustomButton!
     
     
     // MARK: - VC Lifecycle
@@ -46,6 +52,22 @@ class ProfileViewController: UIViewController {
     
     private func fillView() {
         
+        // MARK: Scroll View
+        
+        let scroll = UIScrollView()
+        scroll.translatesAutoresizingMaskIntoConstraints = false
+        scrollView = scroll
+        
+        
+        view.addSubview(scrollView)
+        
+        NSLayoutConstraint.activate([
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
         // MARK: profile Image
         
         let imageView = UIImageView()
@@ -55,15 +77,15 @@ class ProfileViewController: UIViewController {
         
         profileImage = imageView
         
-        view.addSubview(profileImage)
+        scrollView.addSubview(profileImage)
         
         profileImage.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            profileImage.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40),
-            profileImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 41),
-           profileImage.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40),
-           //profileImage.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+            profileImage.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 40),
+            profileImage.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 41),
+           profileImage.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -40),
+           profileImage.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
            profileImage.heightAnchor.constraint(equalTo: profileImage.widthAnchor)
         ])
         
@@ -75,7 +97,7 @@ class ProfileViewController: UIViewController {
         
         setProfileImageButton = setImageButton
         
-        view.addSubview(setProfileImageButton)
+        scrollView.addSubview(setProfileImageButton)
         
         setProfileImageButton.translatesAutoresizingMaskIntoConstraints = false
             
@@ -109,40 +131,17 @@ class ProfileViewController: UIViewController {
         nLabel.text = "Владислав Яндола"
         
         nameLabel = nLabel
-        view.addSubview(nameLabel)
+        scrollView.addSubview(nameLabel)
         
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            nameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40),
-            nameLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40),
+            nameLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 40),
+            nameLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -40),
             nameLabel.topAnchor.constraint(equalTo: profileImage.bottomAnchor, constant: 14)
         ])
         
-        
-        // MARK: editingButton
-        let eButton = UIButton()
-        eButton.setTitle("Редактировать", for: .normal)
-        eButton.setTitleColor(.black, for: .normal)
-        eButton.titleLabel?.font =  eButton.titleLabel?.font.withSize(self.view.frame.width/20)
-        eButton.layer.cornerRadius = 10
-        eButton.layer.borderWidth = 1.5
-        eButton.layer.borderColor = (UIColor.black).cgColor
-        eButton.addTarget(self, action: #selector(editProfileButton(sender:)), for: .touchUpInside)
-        
-        editingButton = eButton
-        view.addSubview(editingButton)
-        
-        editingButton.translatesAutoresizingMaskIntoConstraints = false
-
-        NSLayoutConstraint.activate([
-            editingButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40),
-            editingButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40),
-            editingButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -29),
-            editingButton.widthAnchor.constraint(equalTo: editingButton.heightAnchor, multiplier: 120/17)
-        ])
-        
-        // MARK: descriptionLabel
+        // MARK: descriptionTextView
         
         let dTextView = UITextView()
         dTextView.textAlignment = .center
@@ -155,18 +154,146 @@ class ProfileViewController: UIViewController {
         dTextView.isSelectable = false
 
         descriptionTextView = dTextView
-        view.addSubview(descriptionTextView)
+        scrollView.addSubview(descriptionTextView)
 
         descriptionTextView.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
-            descriptionTextView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            descriptionTextView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            descriptionTextView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 20),
+            descriptionTextView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -20),
             descriptionTextView.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
-            descriptionTextView.bottomAnchor.constraint(equalTo: editingButton.topAnchor, constant: -5)
+            descriptionTextView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.35)
         ])
         
         
+        // MARK: editingButton
+        let eButton = UIButton()
+        eButton.setTitle("Редактировать", for: .normal)
+        eButton.setTitleColor(.black, for: .normal)
+        eButton.titleLabel?.font =  eButton.titleLabel?.font.withSize(self.view.frame.width/20)
+        eButton.layer.cornerRadius = 10
+        eButton.layer.borderWidth = 1.5
+        eButton.layer.borderColor = (UIColor.black).cgColor
+        eButton.addTarget(self, action: #selector(editProfileButton(sender:)), for: .touchUpInside)
+
+        editingButton = eButton
+        scrollView.addSubview(editingButton)
+
+        editingButton.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            editingButton.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 40),
+            editingButton.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -40),
+            editingButton.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -29),
+            editingButton.topAnchor.constraint(equalTo: descriptionTextView.bottomAnchor, constant: 8),
+            editingButton.widthAnchor.constraint(equalTo: editingButton.heightAnchor, multiplier: 120/17)
+        ])
+        
+        
+        
+        // MARK: nameTextField
+        let textField = UITextField()
+        textField.textAlignment = .center
+        textField.font = nameLabel.font
+        textField.text = nameLabel.text
+        textField.layer.cornerRadius = 10
+        textField.backgroundColor = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
+        
+        nameTextField = textField
+        view.addSubview(nameTextField)
+        
+        nameTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            nameTextField.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor),
+            nameTextField.bottomAnchor.constraint(equalTo: nameLabel.bottomAnchor),
+            nameTextField.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            nameTextField.topAnchor.constraint(equalTo: nameLabel.topAnchor)
+        ])
+        
+        nameTextField.isHidden = true
+        
+        
+        // MARK: descriptionTextView
+        let textView = UITextView()
+        textView.textAlignment = .center
+        textView.font = descriptionTextView.font
+        textView.text = descriptionTextView.text
+        textView.layer.cornerRadius = 10
+        textView.backgroundColor = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
+        
+        editingDescriptionTextView = textView
+        view.addSubview(editingDescriptionTextView)
+        
+        editingDescriptionTextView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            editingDescriptionTextView.trailingAnchor.constraint(equalTo: descriptionTextView.trailingAnchor),
+            editingDescriptionTextView.bottomAnchor.constraint(equalTo: descriptionTextView.bottomAnchor),
+            editingDescriptionTextView.leadingAnchor.constraint(equalTo: descriptionTextView.leadingAnchor),
+            editingDescriptionTextView.topAnchor.constraint(equalTo: descriptionTextView.topAnchor)
+        ])
+        
+        editingDescriptionTextView.isHidden = true
+        
+        
+        // MARK: GCDSaveButton
+        let GCDButton = CustomButton()
+        GCDButton.changeBackroundColor(UIColor(red: 61/255, green: 119/255, blue: 236/255, alpha: 1))
+        GCDButton.layer.cornerRadius = editingButton.layer.cornerRadius
+        GCDButton.titleLabel?.font = editingButton.titleLabel?.font
+        GCDButton.setTitle("GCD", for: .normal)
+        GCDButton.setTitleColor(.white, for: .normal)
+        
+        GCDButton.addTarget(self, action: #selector(saveGCDButton(sender:)), for: .touchUpInside)
+        
+        GCDButton.layer.cornerRadius = 10
+        
+        
+        
+        GCDSaveButton = GCDButton
+        view.addSubview(GCDSaveButton)
+        
+        GCDSaveButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            GCDSaveButton.trailingAnchor.constraint(equalTo: view.centerXAnchor, constant: -10),
+            GCDSaveButton.bottomAnchor.constraint(equalTo: editingButton.bottomAnchor),
+            GCDSaveButton.leadingAnchor.constraint(equalTo: editingButton.leadingAnchor),
+            GCDSaveButton.topAnchor.constraint(equalTo: editingButton.topAnchor)
+        ])
+        
+        GCDSaveButton.isHidden = true
+        
+        
+        // MARK: OperationSaveButton
+        
+        let OperationButton = CustomButton()
+        OperationButton.changeBackroundColor(UIColor(red: 61/255, green: 119/255, blue: 236/255, alpha: 1))
+        OperationButton.layer.cornerRadius = editingButton.layer.cornerRadius
+        OperationButton.titleLabel?.font = editingButton.titleLabel?.font
+        OperationButton.setTitle("Operation", for: .normal)
+        OperationButton.setTitleColor(.white, for: .normal)
+        
+        OperationButton.addTarget(self, action: #selector(saveOperationButton(sender:)), for: .touchUpInside)
+        
+        OperationButton.layer.cornerRadius = 10
+        
+        
+        
+        OperationSaveButton = OperationButton
+        view.addSubview(OperationSaveButton)
+        
+        OperationSaveButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            OperationSaveButton.trailingAnchor.constraint(equalTo: editingButton.trailingAnchor),
+            OperationSaveButton.bottomAnchor.constraint(equalTo: editingButton.bottomAnchor),
+            OperationSaveButton.leadingAnchor.constraint(equalTo: view.centerXAnchor, constant: 10),
+            OperationSaveButton.topAnchor.constraint(equalTo: editingButton.topAnchor)
+        ])
+        
+        OperationSaveButton.isHidden = true
         
         
         
@@ -211,8 +338,47 @@ class ProfileViewController: UIViewController {
     }
     
     @objc func editProfileButton(sender: UIButton) {
+            nameTextField.isHidden = !nameTextField.isHidden
+            editingDescriptionTextView.isHidden = !editingDescriptionTextView.isHidden
+            nameLabel.isHidden = !nameLabel.isHidden
+            descriptionTextView.isHidden = !descriptionTextView.isHidden
+
+            descriptionTextView.text = editingDescriptionTextView.text
+
+            editingButton.isHidden = true
+            GCDSaveButton.isHidden = false
+            OperationSaveButton.isHidden = false
+            
+        }
         
-    }
+        @objc func saveGCDButton(sender: UIButton) {
+            nameTextField.isHidden = !nameTextField.isHidden
+            editingDescriptionTextView.isHidden = !editingDescriptionTextView.isHidden
+            nameLabel.isHidden = !nameLabel.isHidden
+            descriptionTextView.isHidden = !descriptionTextView.isHidden
+            
+            descriptionTextView.text = editingDescriptionTextView.text
+            
+            editingButton.isHidden = false
+            OperationSaveButton.isHidden = true
+            GCDSaveButton.isHidden = true
+            
+            
+        }
+        
+        @objc func saveOperationButton(sender: UIButton) {
+            nameTextField.isHidden = !nameTextField.isHidden
+            editingDescriptionTextView.isHidden = !editingDescriptionTextView.isHidden
+            nameLabel.isHidden = !nameLabel.isHidden
+            descriptionTextView.isHidden = !descriptionTextView.isHidden
+            
+            descriptionTextView.text = editingDescriptionTextView.text
+            
+            editingButton.isHidden = false
+            OperationSaveButton.isHidden = true
+            GCDSaveButton.isHidden = true
+            
+        }
     
     
 }
