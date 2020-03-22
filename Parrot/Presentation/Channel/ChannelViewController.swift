9cf6ff493e85329c1056eb8404d5e72688d3ff82
Vlad Yandola <vladan2000.9@mail.ru>
2020-03-22 15:24:28 +0500

    Разделение сообщений на секции по датам

--- Parrot/Presentation/Channel/ChannelViewController.swift
+++ Parrot/Presentation/Channel/ChannelViewController.swift
@@ -27,6 +27,7 @@ class ChannelViewController: UITableViewController {
     }
     
     private var messages: [Message] = []
+    private var groupedMessages: [[Message]] = []
     
     private lazy var db = Firestore.firestore()
     
@@ -42,12 +43,37 @@ class ChannelViewController: UITableViewController {
     private func scrollToBottom() {
         DispatchQueue.main.async { [weak self] in
             if let VC = self {
-                if VC.messages.count > 0 {
+                if VC.groupedMessages.count > 0, let last = VC.groupedMessages.last {
                     usleep(300000)
-                    let indexPath = IndexPath(row: VC.messages.count - 1, section: 0)
+                    let indexPath = IndexPath(row: last.count - 1, section: VC.groupedMessages.count - 1)
                     VC.tableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
                 }
             }
+        }
+    }
+    
+    
+    private func groupMessagesByDate() {
+        let groupedMessages = Dictionary(grouping: messages) { element -> String in
+            let dateFormatter = DateFormatter()
+            dateFormatter.locale = Locale.init(identifier: "ru_RU")
+            dateFormatter.dateFormat = "dd MMM"
+            return dateFormatter.string(from: element.created)
+        }
+        
+        var newGroupMessages: [[Message]] = []
+        groupedMessages.keys.forEach { key in
+            if let values = groupedMessages[key] {
+                newGroupMessages.append(values)
+            }
+        }
+        self.groupedMessages = newGroupMessages.sorted { (values1, values2) -> Bool in
+            if let first1 = values1.first, let first2 = values2.first {
+                if first1.created < first2.created {
+                    return true
+                }
+            }
+            return false
         }
     }
     // MARK: - VC Lifecycle
@@ -68,6 +94,7 @@ class ChannelViewController: UITableViewController {
             }
             return false
         })
+        groupMessagesByDate()
         tableView.reloadData()
         if firstScroll {
             scrollToBottom()
@@ -76,10 +103,15 @@ class ChannelViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return groupedMessages.count
+    }
 
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return messages.count
+        return groupedMessages[section].count
     }
 
     
@@ -88,17 +120,60 @@ class ChannelViewController: UITableViewController {
 
         let isIncoming: Bool
         
-        if messages[indexPath.row].senderID == senderID {
+        if groupedMessages[indexPath.section][indexPath.row].senderID == senderID {
             isIncoming = false
         } else {
             isIncoming = true
         }
-        let currMessage = messages[indexPath.row]
+        let currMessage = groupedMessages[indexPath.section][indexPath.row]
         
         let cellModel = MessageCellModel(text: currMessage.content, date: currMessage.created, senderName: currMessage.senderName, isIncoming: isIncoming)
         cell.configure(with: cellModel)
 
         return cell
     }
+    
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let label = HeaderLabel()
+        label.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.8)
+        label.layer.cornerRadius = 10
+        label.clipsToBounds = true
+        label.textColor = .white
+        label.textAlignment = .center
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale.init(identifier: "ru_RU")
+        dateFormatter.dateFormat = "dd MMM"
+        if let date = groupedMessages[section].first?.created {
+            label.text = dateFormatter.string(from: date)
+        }
+        
+        let conteinerView = UIView()
+        conteinerView.addSubview(label)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        label.centerXAnchor.constraint(equalTo: conteinerView.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: conteinerView.centerYAnchor).isActive = true
+        
+        return conteinerView
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
 
+}
+
+class HeaderLabel : UILabel {
+    override var intrinsicContentSize: CGSize {
+        let originalIntrinsicSize = super.intrinsicContentSize
+        let height = originalIntrinsicSize.height + 12
+        layer.cornerRadius = height/2
+        clipsToBounds = true
+        return CGSize(width: originalIntrinsicSize.width + 20, height: height)
+    }
 }
