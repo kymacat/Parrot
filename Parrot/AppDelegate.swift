d00657679828488f68f09ef51060e6691d79469b
Vlad Yandola <vladan2000.9@mail.ru>
2020-03-28 21:52:26 +0500

    Создал модель данных для хранения информации профиля и инициализировал Core Data Stack

--- Parrot/AppDelegate.swift
+++ Parrot/AppDelegate.swift
@@ -32,56 +32,42 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         navigationBarAppearace.tintColor = UIColor.black
             
         FirebaseApp.configure()
-        return true
-    }
-    
-    
-    
-
-    // MARK: - Core Data stack
-
-    lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-        */
-        let container = NSPersistentContainer(name: "Parrot")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+        
+        
+        let manager = CoreDataFileManager()
+        
+        
+        // Извление записей
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "ProfileInformation")
+        do {
+            let results = try manager.managedObjectContext.fetch(fetchRequest)
+            if results.count == 0 {
+                if let entityDescription = NSEntityDescription.entity(forEntityName: "ProfileInformation", in: manager.managedObjectContext) {
+                    
+                    let managedObject = NSManagedObject(entity: entityDescription, insertInto: manager.managedObjectContext)
+                    managedObject.setValue("Влад", forKey: "name")
+                    managedObject.setValue("Люблю программировать под iOS, изучать что-то новое и не стоять на месте", forKey: "userDescription")
+                    if let imageData = UIImage(named: "placeholder-user")?.pngData() {
+                        managedObject.setValue(imageData, forKey: "imageData")
+                    }
+                    manager.saveContext()
+                    
+                    
+                    
+                }
+                
+            } else {
+                for result in results as! [NSManagedObject] {
+                    print("name - \(result.value(forKey: "name")!)")
+                    print("userDescription - \(result.value(forKey: "userDescription")!)")
+                    print("imageData - \(result.value(forKey: "imageData")!)")
+                }
             }
-        })
-        return container
-    }()
-
-    // MARK: - Core Data Saving support
-
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
+        } catch {
+            print(error)
         }
+        
+        return true
     }
 
 }
-
